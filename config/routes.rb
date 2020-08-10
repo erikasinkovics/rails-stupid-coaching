3ccae0570509624f4d1b9052d2b4816f9aeeae96
Erika Sinkovics <erika.sinkovics@outlook.com>
2020-08-10 14:36:37 -0400

    Setting up base structure for controllers/views/router.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/ask", to: "questions#ask"
+  get "/ask", to: "questions#ask", as: "ask"
+  get "/answer", to: "questions#answer", as: "answer"
+  get "/", to: "questions#home", as: "home"
 end
